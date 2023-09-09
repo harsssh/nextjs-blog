8492101c54d58a615e51e9428ddbf4b0172768ad
harsh <66548698+harsssh@users.noreply.github.com>
2023-09-09 23:48:29 +0900

    link pages

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,13 @@
+import Link from "next/link";
+
 // default exportすれば, 関数名はなんでもOK
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">Back to home</Link>
+        </h2>
+    </>
+  ) 
 }
