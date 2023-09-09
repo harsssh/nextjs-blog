a3b58562f96fc1375996a947bd6da4883a8b4027
harsh <66548698+harsssh@users.noreply.github.com>
2023-09-10 00:18:02 +0900

    add profile image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
 
 // default exportすれば, 関数名はなんでもOK
 export default function FirstPost() {
   return (
     <>
-        <h1>First Post</h1>
-        <h2>
-            <Link href="/">Back to home</Link>
-        </h2>
+      <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg"
+        height={144}
+        width={144}
+        alt="profile image"
+      />
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
     </>
-  ) 
+  );
 }
