827b54e615d1d561b65f705317626084f39d5963
harsh <66548698+harsssh@users.noreply.github.com>
2023-09-10 15:12:07 +0900

    wip

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,27 @@
+import { Head } from "next/head";
 import styles from "./layout.module.css";
 
-export default function Layout({ children }) {
+const name = "harsssh";
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout({ children, home }) {
   return (
-    <>
-      <div className={styles.container}>{children}</div>
-    </>
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle}></meta>
+        <meta name="twitter:card" content="summary_large_image"></meta>
+      </Head>
+    </div>
   );
 }
